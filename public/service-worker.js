c0cd9368a7630eedb16a6db4fbad9e3f5cb5ccc1
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-04 11:17:35 +0100

    fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,17 +8,26 @@ const filesToCache = [
     '/js/preload.js'
 ]
 
-const staticCacheName = 'pages-cache-1'
+self.addEventListener('activate', event => {
+    console.log('Service worker activated')
+})
 
 self.addEventListener('install', event => {
     console.log('Attempting to install service worker and cache static assets')
     event.waitUntil(
-        caches.open(staticCacheName).then(cache => {
+        caches.open('pages-cache-1').then(cache => {
             return cache.addAll(filesToCache)
         })
     )
 })
 
-self.addEventListener('activate', function(event) {
-    console.log('Service worker activated')
+self.addEventListener('fetch', event => {
+    console.log('Fetching from cache')
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) return response
+            return fetch(event.request)
+        })
+    )
 })
