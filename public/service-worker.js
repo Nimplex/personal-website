953a6d4661442f5bacd222d2697c9542469b708d
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-04 11:06:03 +0100

    caching home page

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,24 @@
-self.addEventListener('install', function(event) {
-    console.log('Service worker installed')
+const filesToCache = [
+    '/',
+    '/manifest.json',
+    '/css/style.css',
+    '/css/navbar.css',
+    '/assets/fonts/Montserrat-Regular.ttf',
+    '/js/main.js',
+    '/js/preload.js'
+]
+
+const staticCacheName = 'pages-cache-1'
+
+self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets')
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            return cache.addAll(filesToCache)
+        })
+    )
 })
+
 self.addEventListener('activate', function(event) {
     console.log('Service worker activated')
 })
